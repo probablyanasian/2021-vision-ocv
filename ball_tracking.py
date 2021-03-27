84148333448eb6e5ec61606060d5a967404cb088
probablyanasian <43358667+probablyanasian@users.noreply.github.com>
2021-03-27 15:15:23 -0700

    track largest with queue

--- ball_tracking.py
+++ ball_tracking.py
@@ -90,6 +90,12 @@ while True:
 				cv2.circle(frame, (int(x), int(y)), int(radius),
 					(0, 255, 255), 2)
 				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+		
+		#track the largest
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 	# update the points queue
 	pts.appendleft(center)
